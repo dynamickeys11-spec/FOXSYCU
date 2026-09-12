2138c480e39124260dc7855389109f0046ad9b4f
dynamickeys11-spec <dynamickeys11@gmail.com>
2026-09-12 08:45:42 +0100

    fix: route customer services through canonical live views

--- src/UnifiedServicesFixed.tsx
+++ src/UnifiedServicesFixed.tsx
@@ -1,5 +1,5 @@
 import { BankingShell } from './BankingShell'
-import { SecurityCenter } from './SecurityPremiumFixed'
+import { SecurityCenter, PrivateBanking } from './SecurityPremiumFixed'
 import { BeneficiariesPage, SavingsPage, SettingsPage } from './CoreBankingServices'
 import { TransferCenterV3 } from './TransferCenterV3'
 import { CommunicationCenterV2 } from './FeatureBanking'
@@ -11,14 +11,10 @@ function withShell(Page: React.ComponentType) { return function UnifiedServicePa
 
 export const UnifiedSecurityCenter = withShell(SecurityCenter)
 export const UnifiedCommunicationCenter = CommunicationCenterV2
-export const UnifiedPrivateBanking = withShell(PrivateBankingPlaceholder)
+export const UnifiedPrivateBanking = withShell(PrivateBanking)
 export const UnifiedBeneficiaries = withShell(BeneficiariesPage)
 export const UnifiedSavings = withShell(SavingsPage)
 export const UnifiedCards = CardsCenterV5
 export const UnifiedStatements = StatementsCenterV3
 export const UnifiedSettings = withShell(SettingsPage)
 export const UnifiedTransfers = TransferCenterV3
-
-function PrivateBankingPlaceholder() {
-  return <div className="core-page"><div className="core-heading"><div><span>PRIVATE BANKING</span><h1>Private Banking</h1><p>Relationship services are being consolidated into the canonical banking shell.</p></div></div></div>
-}
