3b3ba1f9c52f39d1aa8aace7f0932d8d6def7d16
dynamickeys11-spec <dynamickeys11@gmail.com>
2026-09-12 05:09:09 +0100

    fix: restore stable banking shell before UX rebuild

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './WorldBanking'
+import App from './AppLedger'
 import './styles.css'
-import './world.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
