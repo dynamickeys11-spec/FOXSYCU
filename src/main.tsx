077de1855aaee64d5485986bb7c3fadd7c2b1b27
dynamickeys11-spec <dynamickeys11@gmail.com>
2026-09-12 05:32:38 +0100

    feat: route security communication and private banking center

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, useLocation } from 'react-router-dom'
 import App from './WorldBankingV2'
+import { CommunicationCenter, PrivateBanking, SecurityCenter } from './SecurityPremium'
 import './styles.css'
+
+function Root() {
+  const location = useLocation()
+  if (location.pathname === '/security') return <SecurityCenter />
+  if (location.pathname === '/communication') return <CommunicationCenter />
+  if (location.pathname === '/private-banking') return <PrivateBanking />
+  return <App />
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Root />
     </BrowserRouter>
   </StrictMode>,
 )
