c316ef0108a15ec6766ffe1eaabc13f90eebb439
dynamickeys11-spec <dynamickeys11@gmail.com>
2026-09-12 05:46:17 +0100

    feat: wrap service areas in shared banking shell

---
+++ src/UnifiedServices.tsx
@@ -0,0 +1,19 @@
+import { BankingShell } from './BankingShell'
+import { CommunicationCenter, PrivateBanking, SecurityCenter } from './SecurityPremium'
+import { BeneficiariesPage, SavingsPage, CardsPage, StatementsPage, SettingsPage } from './CoreBankingServices'
+import './unified-services.css'
+
+function withShell(Page: React.ComponentType) {
+  return function UnifiedServicePage() {
+    return <BankingShell><Page /></BankingShell>
+  }
+}
+
+export const UnifiedSecurityCenter = withShell(SecurityCenter)
+export const UnifiedCommunicationCenter = withShell(CommunicationCenter)
+export const UnifiedPrivateBanking = withShell(PrivateBanking)
+export const UnifiedBeneficiaries = withShell(BeneficiariesPage)
+export const UnifiedSavings = withShell(SavingsPage)
+export const UnifiedCards = withShell(CardsPage)
+export const UnifiedStatements = withShell(StatementsPage)
+export const UnifiedSettings = withShell(SettingsPage)
