5b6e7dd4645692d71302cccc08175e8b0fae3353
dynamickeys11-spec <dynamickeys11@gmail.com>
2026-09-12 08:00:38 +0100

    feat: route Build 5 cards center

--- src/UnifiedServicesFixed.tsx
+++ src/UnifiedServicesFixed.tsx
@@ -2,22 +2,17 @@ import { BankingShell } from './BankingShell'
 import { SecurityCenter, PrivateBanking } from './SecurityPremiumFixed'
 import { SettingsPage } from './CoreBankingServices'
 import { TransferCenterV3 } from './TransferCenterV3'
-import { SavingsCenter, CardsCenter, BeneficiariesCenter, CommunicationCenterV2 } from './FeatureBanking'
+import { SavingsCenter, BeneficiariesCenter, CommunicationCenterV2 } from './FeatureBanking'
+import { CardsCenterV5 } from './CardsCenterV5'
 import { StatementsCenterV3 } from './StatementsCenterV3'
 import './unified-services.css'
-
-function withShell(Page: React.ComponentType) {
-  return function UnifiedServicePage() {
-    return <BankingShell><Page /></BankingShell>
-  }
-}
-
-export const UnifiedSecurityCenter = withShell(SecurityCenter)
-export const UnifiedCommunicationCenter = CommunicationCenterV2
-export const UnifiedPrivateBanking = withShell(PrivateBanking)
-export const UnifiedBeneficiaries = BeneficiariesCenter
-export const UnifiedSavings = SavingsCenter
-export const UnifiedCards = CardsCenter
-export const UnifiedStatements = StatementsCenterV3
-export const UnifiedSettings = withShell(SettingsPage)
-export const UnifiedTransfers = TransferCenterV3
+function withShell(Page: React.ComponentType) { return function UnifiedServicePage(){ return <BankingShell><Page /></BankingShell> } }
+export const UnifiedSecurityCenter=withShell(SecurityCenter)
+export const UnifiedCommunicationCenter=CommunicationCenterV2
+export const UnifiedPrivateBanking=withShell(PrivateBanking)
+export const UnifiedBeneficiaries=BeneficiariesCenter
+export const UnifiedSavings=SavingsCenter
+export const UnifiedCards=CardsCenterV5
+export const UnifiedStatements=StatementsCenterV3
+export const UnifiedSettings=withShell(SettingsPage)
+export const UnifiedTransfers=TransferCenterV3
