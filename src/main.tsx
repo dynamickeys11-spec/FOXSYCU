142ef749f4476eebd5846ed5edb8a33ed6cc85f9
dynamickeys11-spec <dynamickeys11@gmail.com>
2026-09-12 05:46:26 +0100

    fix: unify service routes with primary banking shell

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, useLocation } from 'react-router-dom'
 import App from './WorldBankingV2'
-import { CommunicationCenter, PrivateBanking, SecurityCenter } from './SecurityPremium'
-import { BeneficiariesPage, SavingsPage, CardsPage, StatementsPage, SettingsPage } from './CoreBankingServices'
+import {
+  UnifiedCommunicationCenter,
+  UnifiedPrivateBanking,
+  UnifiedSecurityCenter,
+  UnifiedBeneficiaries,
+  UnifiedSavings,
+  UnifiedCards,
+  UnifiedStatements,
+  UnifiedSettings,
+} from './UnifiedServices'
 import './styles.css'
 
 function Root() {
   const location = useLocation()
-  if (location.pathname === '/security') return <SecurityCenter />
-  if (location.pathname === '/communication') return <CommunicationCenter />
-  if (location.pathname === '/private-banking') return <PrivateBanking />
-  if (location.pathname === '/beneficiaries') return <BeneficiariesPage />
-  if (location.pathname === '/savings') return <SavingsPage />
-  if (location.pathname === '/cards') return <CardsPage />
-  if (location.pathname === '/statements') return <StatementsPage />
-  if (location.pathname === '/settings') return <SettingsPage />
+  if (location.pathname === '/security') return <UnifiedSecurityCenter />
+  if (location.pathname === '/communication') return <UnifiedCommunicationCenter />
+  if (location.pathname === '/private-banking') return <UnifiedPrivateBanking />
+  if (location.pathname === '/beneficiaries') return <UnifiedBeneficiaries />
+  if (location.pathname === '/savings') return <UnifiedSavings />
+  if (location.pathname === '/cards') return <UnifiedCards />
+  if (location.pathname === '/statements') return <UnifiedStatements />
+  if (location.pathname === '/settings') return <UnifiedSettings />
   return <App />
 }
 
